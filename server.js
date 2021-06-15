89601a3975d7d77c77d2d15887efa2a2a67810c0
FerasSadTawfiqEzz-aldeen <Ferasezaldeen@gmail.com>
2021-06-15 23:15:43 +0300

    finish lab8

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 'use strict';
 const express= require('express');
 const server=express();
-const weatherData=require('./data/weather.json');
+
 const cors = require('cors');
-require('dotenv').config()
+const { default: axios } = require('axios');
+require('dotenv').config();
 server.use(cors());
 
 
 
 
-const PORT=process.env.PORT || 1996;
+const PORT=process.env.PORT;
 
 
 
@@ -20,42 +21,68 @@ class Forecast {
     this.date= object.valid_date;
 
   }
-};
+}
 
-let arr=weatherData;
-delete arr.data;
+class Movies {
+  constructor(object){
 
-server.listen(PORT,()=>{
+    this.title=object.title;
+    this.overview=object.overview;
+    this.average_votes=object.vote_average;
+    this.total_votes=object.vote_count;
+    this.image_url=`https://image.tmdb.org/t/p/w500${object.poster_path}`;
+    this.popularity=object.popularity;
+    this.released_on=object.release_date;
+  }
+
+
+}
+
+
+
+server.listen(1996,()=>{
   console.log(`im using port ${PORT}`);
 });
 
 // localhost:1996/weather?lat=123&?lon=123&?searchQuery=cityNmae
-server.get('/weather',(req,res)=>{
-  let latreq=req.query.lat;
-  let lonreq=req.query.lon;
+server.get('/weather', getWeather);
+server.get('/movies',getMovie);
+
+function getWeather (req,res){
+  // let latreq=req.query.lat;
+  // let lonreq=req.query.lon;
   let searchQueryreq=req.query.searchQuery;
-  let weatherItem= weatherData.find(item=>{
-    if((item.lat==latreq&&item.lon==lonreq)||( item.city_name==searchQueryreq.toLowerCase()))
-    return item;
-
-  });
-
-  try{
-    let forecasts = weatherItem.data.map(item => {
-
-       { return new Forecast(item);}
+  let KEY=process.env.WEATHER_API_KEY;
+  let url=`https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQueryreq}&key=${KEY}`;
+  axios.get(url).then(apiResults=>{
+    const forecasts = apiResults.data.data.map(item => {
+      { return new Forecast(item);}
     });
     res.send(forecasts);
-  }
-  catch
-  {
-    res.status(500).send('OPS!! Your City Not Found');
-  }
-
-    
 
 
-});
+  });
+}
+
+
+function getMovie(req,res){
+  let searchQueryreq=req.query.searchQuery;
+  let KEY=process.env.MOVIE_API_KEY;
+  let url=`https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query==${searchQueryreq}`;
+  axios.get(url).then(apiResults=>{
+    const movInfo= apiResults.data.results.map(item =>{
+      {return new Movies(item);}
+    });
+    res.send(movInfo);
+
+  })
+    .catch(err =>{
+      res.send(`there is an error in getting the data => ${err}`);});
+
+
+}
+
+
 
 
 server.get('*',(req,res) =>{
